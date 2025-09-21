b06209b44461ae4c28dc09c1f4d7d21de0dd1063
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2025-09-21 17:57:08 +0200

    added git, lazygit config

--- private_dot_config/fish/conf.d/lazygit/session_vars.fish
+++ private_dot_config/fish/conf.d/lazygit/session_vars.fish
@@ -1,5 +1,5 @@
 if not set -q __LAZYGIT_VARS_SOURCED
     set -xg __LAZYGIT_VARS_SOURCED 1
 
-    set -xg LG_CONFIG_FILE $XDG_CONFIG_HOME/lazygit/config.yml
+    set -xg LG_CONFIG_FILE $XDG_CONFIG_HOME/lazygit/config.yml,$XDG_CONFIG_HOME/lazygit/mauve.yml
 end
