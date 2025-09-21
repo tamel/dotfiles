40d5bdc0ea6319e2410a59178115b00539262470
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2025-09-21 23:11:14 +0200

    added nvim keybind to dismiss notifications

--- private_dot_config/nvim/lua/plugins/notify.lua
+++ private_dot_config/nvim/lua/plugins/notify.lua
@@ -1,10 +1,14 @@
 return {
-	"rcarriga/nvim-notify",
-	opts = {
-		background_colour = "#000000",
-		maxWidth = 200,
-		render = "wrapped-compact",
-		stages = "slide",
-	},
+  "rcarriga/nvim-notify",
+  lazy = false,
+  main = "notify",
+  keys = {
+    { "<leader>nd", function() require("notify").dismiss() end, desc = "Dismiss notifications" },
+  },
+  opts = {
+    background_colour = "#000000",
+    maxWidth = 200,
+    render = "wrapped-compact",
+    stages = "slide",
+  },
 }
-
