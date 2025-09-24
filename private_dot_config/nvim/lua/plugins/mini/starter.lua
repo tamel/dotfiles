9e2d805b1770ef1caa2a2f98efd0d66c519a3ec5
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2025-09-24 02:36:40 +0200

    changed minit starter config

--- private_dot_config/nvim/lua/plugins/mini/starter.lua
+++ private_dot_config/nvim/lua/plugins/mini/starter.lua
@@ -1,28 +1,31 @@
 return {
-	"nvim-mini/mini.starter",
-	version = "*",
-	config = function()
-		local starter = require("mini.starter")
-		starter.setup({
+  "nvim-mini/mini.starter",
+  version = "*",
+  config = function()
+    local starter = require("mini.starter")
+    starter.setup({
 
-			header = "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗\n"
-				.. "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║\n"
-				.. "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║\n"
-				.. "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║\n"
-				.. "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║\n"
-				.. "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝\n",
-			footer = "",
-			items = {
-				{ action = "enew", name = "New Buffer", section = "Actions" },
-				{ action = "qall", name = "Quit", section = "Actions" },
-				{ action = "lua MiniFiles.open()", name = "Browse", section = "Files" },
-				{ action = "lua Snacks.picker.files()", name = "Find Files", section = "Files" },
-				starter.sections.recent_files,
-			},
-			content_hooks = {
-				starter.gen_hook.adding_bullet(),
-				starter.gen_hook.aligning("center", "center"),
-			},
-		})
-	end,
+      header = " _   _  _____ _____  _   _ ________  ___\n"
+          .. "| \\ | ||  ___|  _  || | | |_   _|  \\/  |\n"
+          .. "|  \\| || |__ | | | || | | | | | | .  . |\n"
+          .. "| . ` ||  __|| | | || | | | | | | |\\/| |\n"
+          .. "| |\\  || |___\\ \\_/ /\\ \\_/ /_| |_| |  | |\n"
+          .. "\\_| \\_/\\____/ \\___/  \\___/ \\___/\\_|  |_/\n",
+
+
+
+      footer = "",
+      items = {
+        { action = "enew",                      name = "New Buffer", section = "Actions" },
+        { action = "qall",                      name = "Quit",       section = "Actions" },
+        { action = "lua MiniFiles.open()",      name = "Browse",     section = "Files" },
+        { action = "lua Snacks.picker.files()", name = "Find Files", section = "Files" },
+        starter.sections.recent_files,
+      },
+      content_hooks = {
+        starter.gen_hook.adding_bullet(),
+        starter.gen_hook.aligning("center", "center"),
+      },
+    })
+  end,
 }
