539402deb52937fb71c4e9d558d2a53b0994bef8
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2025-09-26 13:55:40 +0200

    fixed check_dependencies script

--- private_dot_config/fish/functions/check_dependencies.fish
+++ private_dot_config/fish/functions/check_dependencies.fish
@@ -1,5 +1,5 @@
 function check_dependencies
-    for file in $XDG_CONFIG_HOME/fish/conf.d/*/check_dependencies.fish
+    for file in $HOME/.config/fish/conf.d/*/check_dependencies.fish
         echo checking (basename (dirname $file))\:
         fish $file
     end
