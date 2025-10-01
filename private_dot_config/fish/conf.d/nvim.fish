e044046c3f7fa94a8236bf48ada123bf2105a352
Thomas Stichlmair <thomas.stichlmair.ext@zeiss.com>
2025-10-01 17:33:14 +0200

    added detection for /opt nvim

--- private_dot_config/fish/conf.d/nvim.fish
+++ private_dot_config/fish/conf.d/nvim.fish
@@ -1,5 +1,8 @@
 function setup_nvim
     if command -q nvim
         source $HOME/.config/fish/conf.d/nvim/session_vars.fish
+    else if test -x /opt/nvim-linux-x86_64/bin/nvim
+        fish_add_path /opt/nvim-linux-x86_64/bin
+        source $HOME/.config/fish/conf.d/nvim/session_vars.fish
     end
 end
