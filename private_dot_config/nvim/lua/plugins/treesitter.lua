0d63960234189a8211501538ebb419b03c15d19c
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2026-04-23 00:27:43 +0200

    nvim: pinned treesitter to master branch
    support up to nvim 11

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
+  branch = "master",
+  lazy = false,
   build = ":TSUpdate",
   main = "nvim-treesitter.configs",
   opts = {
